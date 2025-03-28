860b672b78ece415e5522cd6cd7be4b3fdce15ae
Pratik Somwanshi <pratiksomwanshi570@gmail.com>
2025-03-28 15:53:02 +0530

    add hover border for terminal effect

--- components/local/dock/index.tsx
+++ components/local/dock/index.tsx
@@ -80,7 +80,7 @@ function DockLocal() {
                                 </Link>
                             </TooltipTrigger>
                             <TooltipContent className="bg-gray-800 text-white px-3 py-2 rounded-md shadow-md relative">
-                                <p>LinkedIn</p>
+                                <p>Contact</p>
                                 <div className="absolute z-10 w-3 h-3 bg-gray-800 rotate-45 -bottom-1 left-1/2 -translate-x-1/2" />
                             </TooltipContent>
                         </Tooltip>
--- components/local/home/terminal_card/index.tsx
+++ components/local/home/terminal_card/index.tsx
@@ -1,50 +1,54 @@
 "use client";
+import { MagicCard } from "@/components/magicui/magic-card";
 import {
     AnimatedSpan,
     Terminal,
     TypingAnimation,
 } from "@/components/magicui/terminal";
+import { Card } from "@/components/ui/card";
 import React from "react";
 
 function TerminalCardLocal() {
     return (
-        <Terminal className="h-[18rem]">
-            <TypingAnimation className="text-muted-foreground">
-                &gt; whoami
-            </TypingAnimation>
+        <MagicCard className="p-1 rounded-xl" gradientColor={"#262626"}>
+            <Terminal className="h-[18rem]">
+                <TypingAnimation className="text-muted-foreground">
+                    &gt; whoami
+                </TypingAnimation>
 
-            <AnimatedSpan delay={1500} className="text-green-500">
-                <span>✔ I am a Computer Engineering graduate</span>
-            </AnimatedSpan>
+                <AnimatedSpan delay={1500} className="text-green-500">
+                    <span>✔ I am a Computer Engineering graduate</span>
+                </AnimatedSpan>
 
-            <AnimatedSpan delay={2000} className="text-green-500">
-                <span>✔ CGPA: 8.19</span>
-            </AnimatedSpan>
+                <AnimatedSpan delay={2000} className="text-green-500">
+                    <span>✔ CGPA: 8.19</span>
+                </AnimatedSpan>
 
-            <AnimatedSpan delay={2500} className="text-green-500">
-                <span>✔ From: Dr. D Y Patil SOET, Pune</span>
-            </AnimatedSpan>
+                <AnimatedSpan delay={2500} className="text-green-500">
+                    <span>✔ From: Dr. D Y Patil SOET, Pune</span>
+                </AnimatedSpan>
 
-            <AnimatedSpan delay={3000} className="text-green-500">
-                <span>✔ Proficient in: Java, JavaScript, TypeScript</span>
-            </AnimatedSpan>
+                <AnimatedSpan delay={3000} className="text-green-500">
+                    <span>✔ Proficient in: Java, JavaScript, TypeScript</span>
+                </AnimatedSpan>
 
-            <AnimatedSpan delay={3500} className="text-green-500">
-                <span>✔ Frontend: React.js, Next.js, Redux</span>
-            </AnimatedSpan>
+                <AnimatedSpan delay={3500} className="text-green-500">
+                    <span>✔ Frontend: React.js, Next.js, Redux</span>
+                </AnimatedSpan>
 
-            <AnimatedSpan delay={4000} className="text-green-500">
-                <span>✔ Backend: Spring Boot, Node.js, Express.js</span>
-            </AnimatedSpan>
+                <AnimatedSpan delay={4000} className="text-green-500">
+                    <span>✔ Backend: Spring Boot, Node.js, Express.js</span>
+                </AnimatedSpan>
 
-            <AnimatedSpan delay={4500} className="text-green-500">
-                <span>✔ Database: MySQL, MongoDB</span>
-            </AnimatedSpan>
+                <AnimatedSpan delay={4500} className="text-green-500">
+                    <span>✔ Database: MySQL, MongoDB</span>
+                </AnimatedSpan>
 
-            <AnimatedSpan delay={5000} className="text-green-500">
-                <span>✔ Deployment: Docker, AWS</span>
-            </AnimatedSpan>
-        </Terminal>
+                <AnimatedSpan delay={5000} className="text-green-500">
+                    <span>✔ Deployment: Docker, AWS</span>
+                </AnimatedSpan>
+            </Terminal>
+        </MagicCard>
     );
 }
 
